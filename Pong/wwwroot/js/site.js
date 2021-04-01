992a85c9a57ed66f5d8d2b97714ac3e07dc980ca
Slothie <Slothie@outlook.com.au>
2021-04-02 09:39:07 +1000

    Add project files.

---
+++ Pong/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+(function () {
+
+    const canvas = document.getElementsByClassName('canvas')[0],
+        c = canvas.getContext('2d');
+    var x = 0;
+
+    function render() {
+        c.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+        c.beginPath();
+
+        // Adjust path based on x position
+        c.moveTo(x, 50);
+        c.quadraticCurveTo(x, 0, x + 50, 0);
+        c.quadraticCurveTo(x + 100, 0, x + 100, 50);
+        c.quadraticCurveTo(x + 100, 100, x + 50, 100);
+        c.quadraticCurveTo(x, 100, x, 50);
+        c.stroke();
+
+        // Doing this for animation
+        x += 5;
+        if (x > canvas.width) {
+            x = -100;
+        }
+        requestAnimationFrame(render);
+    }
+    render();
+})();
