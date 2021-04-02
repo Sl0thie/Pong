f4fc6605053c341f527999fc4c6013fb54047935
Slothie <Slothie@outlook.com.au>
2021-04-02 10:23:41 +1000

    Added Render

--- Pong/wwwroot/js/site.js
+++ Pong/wwwroot/js/site.js
@@ -1,29 +1,66 @@
 ﻿'use strict';
+var canvas;
+var ctx;
+var x = 0;
 
-(function () {
+var centerX = 600;
+var centerY = 600;
+var speed = 1;
 
-    const canvas = document.getElementsByClassName('canvas')[0],
-        c = canvas.getContext('2d');
-    var x = 0;
+window.onload = function(){
+    initialise();
+}
 
-    function render() {
-        c.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-        c.beginPath();
-
-        // Adjust path based on x position
-        c.moveTo(x, 50);
-        c.quadraticCurveTo(x, 0, x + 50, 0);
-        c.quadraticCurveTo(x + 100, 0, x + 100, 50);
-        c.quadraticCurveTo(x + 100, 100, x + 50, 100);
-        c.quadraticCurveTo(x, 100, x, 50);
-        c.stroke();
-
-        // Doing this for animation
-        x += 5;
-        if (x > canvas.width) {
-            x = -100;
-        }
-        requestAnimationFrame(render);
-    }
+function initialise() {
+    window.addEventListener('keydown', onKeyDown);
+    canvas = document.getElementsByClassName('canvas')[0],ctx = canvas.getContext('2d');
+    window.addEventListener("resize", resize);
+    resize();
     render();
-})();
+}
+
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+    ctx.beginPath();
+
+    //Player
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillRect(100, 100, 30, 100);
+
+    //Opponent
+    ctx.fillStyle = '#FFFFF';
+    ctx.fillRect(500, 100, 30, 100);  
+    
+    //Ball
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, 20, 0, 2 * Math.PI, false);
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#FFFFF';
+    ctx.stroke();
+
+    // Doing this for animation
+    centerX += speed;
+    if (centerX > canvas.width) {
+        centerX = -100;
+        speed++;
+    }
+    requestAnimationFrame(render);
+}
+
+function resize() {
+
+    ctx.canvas.width = canvas.clientWidth;
+    ctx.canvas.height = canvas.clientHeight;
+}
+
+function onKeyDown(event) {
+    var keyCode = event.keyCode;
+    console.log(keyCode);
+    event.preventDefault();
+
+    //left a=65 z=90
+
+    //right "=222 /=191
+};
