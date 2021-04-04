c89a55abab6f95ad7313c755c4385f1dc8add2c3
Dallas Adams <dallasadams@hotmail.com>
2021-04-04 14:09:17 +1000

    Farm end.

--- Pong/wwwroot/js/site.js
+++ Pong/wwwroot/js/site.js
@@ -1,24 +1,41 @@
 ﻿'use strict';
-var canvas;              //Canvas object.
-var ctx;                 //Canvas context object.
-var playerscore;         //Player score object.
-var opponentscore;       //Opponent score object.
-var playerScore = 0;     //Player's game score.
-var opponentScore = 0;   //Opponent's game score.
-var ballRadius = 20;     //Radius of the ball.
-var x = 0;
-var angle = 0;
-var width;               //Width of the canvas.
-var height;              //Height of the canvas.
-var centerX;             //Ball X-axis position.
-var centerY;             //Ball Y-axis position.
-var speed = 1;           //Speed of the ball. (x-axis)
-var direction = 1;       //Direction of the ball. (x-axis)
-const maxSpeed = 10;     //Maximum speed of the ball. (x-axis)
-const paddleSpeed = 10;  //Players paddle speed.
-var playerY;
-var opponentY;
-let keysPressed = {};
+const minSpeed = 5;                 //Maximum speed of the ball. (x-axis)
+const maxSpeed = 40;                 //Maximum speed of the ball. (x-axis)
+const paddleSpeed = 15;              //Players paddle speed.
+const forward = 1;                   //X-axis positive direction.
+const reverse = -1                   //X-axis negative direction.
+const startAngle = 0;                //The starting angle for the ball's arc.
+const endAngle = Math.PI * 2;        //The finishing angle for the ball's arc.
+const ballRadius = 20;               //Radius of the ball.
+const ballDiameter = ballRadius * 2; //Diameter of the ball.
+let canvas;                          //Canvas object.
+let ctx;                             //Canvas context object.
+let playerscore;                     //Player score object.
+let opponentscore;                   //Opponent score object.
+let playerScore = 0;                 //Player's game score.
+let opponentScore = 0;               //Opponent's game score.
+let angle = 0;                       //Angle is the Y-axis movement. (its not really an angle atm)
+let width;                           //Width of the canvas.
+let height;                          //Height of the canvas.
+let centerX;                         //Ball X-axis position.
+let centerY;                         //Ball Y-axis position.
+let speed = minSpeed;                //Speed of the ball. (x-axis)
+let direction = 1;                   //Direction of the ball. (x-axis)
+let paddleOffset = 100;              //The offset distance from the edge of the display to the paddle.
+let playerY;                         //Player's paddle location. (top edge)    
+let playerPaddleFront;               //X-axis location of the front of the player's paddle.
+let playerPaddleBack;                //X-axis location of the back of the player's paddle.
+let playerPaddleHeight = 100;        //The height of the player's paddle.
+let playerPaddleWidth = 20;          //The width of the player's paddle.
+let opponentY;                       //Opponent's paddle location.
+let opponentPaddleFront;             //X-axis location of the front of the opponent's paddle.
+let opponentPaddleBack;              //X-axis location of the back of the opponent's paddle.
+let opponentPaddleHeight = 100;      //The height of the player's paddle.
+let opponentPaddleWidth = 20;        //The width of the opponent's paddle.
+let keysPressed = {};                //Array to handle multiple keydown events at the same time.
+
+let ping1 = new Audio('/sounds/1.wav');
+let ping2 = new Audio('/sounds/2.wav');
 
 window.onload = function(){
     initialise();
@@ -33,7 +50,6 @@ document.addEventListener('keyup', (event) => {
 });
 
 function initialise() {
-    //window.addEventListener('keydown', onKeyDown);
     canvas = document.getElementsByClassName('canvas')[0], ctx = canvas.getContext('2d');
     playerscore = document.getElementById('playerscore')
     opponentscore = document.getElementById('opponentscore')
@@ -48,44 +64,45 @@ function render() {
     if (keysPressed['\'']) { opponentY = opponentY - paddleSpeed; }
     if (keysPressed['/']) { opponentY = opponentY + paddleSpeed; }
     if (playerY <= 0) { playerY = 0; }
-    if (playerY >= height - 100) { playerY = height - 100; }
+    if (playerY >= height - playerPaddleHeight) { playerY = height - playerPaddleHeight; }
     if (opponentY <= 0) { opponentY = 0; }
-    if (opponentY >= height - 100) { opponentY = height - 100; }
+    if (opponentY >= height - opponentPaddleHeight) { opponentY = height - opponentPaddleHeight; }
     if (centerY < ballRadius) { angle = -angle; }
     if (centerY > height - ballRadius) { angle = -angle; }
 
     centerX += speed * direction;
     centerY += speed * angle;
 
-    if (direction === 1) {
-        if ((centerX > width - 140) && (centerX < width - 120)){
+    if (direction === forward) {
+        if ((centerX > opponentPaddleFront - ballRadius) && (centerX < opponentPaddleBack)){
             //check for hit.
-            if ((centerY > opponentY) && (centerY < opponentY + 100)) {
-                direction = -1;
-                speed++;
+            if ((centerY > opponentY - ballRadius) && (centerY < opponentY + opponentPaddleHeight + ballRadius)) {
+                ping1.play();
+                direction = reverse;
                 angle = angle + ((opponentY - centerY - 50) * 0.001);
-               
+                speed++;
                 if (speed > maxSpeed) {
                     speed = maxSpeed;
                 }
             }
         }
-        if (centerX > width + 40) {
+        if (centerX > width + ballDiameter) {
             playerScored();
         }
     }
     else {
-        if ((centerX < 140) && (centerX > 120)) {
-            if ((centerY > playerY) && (centerY < playerY + 100)) {
-                direction = 1;
-                speed++;
+        if ((centerX < playerPaddleFront + ballRadius) && (centerX > playerPaddleBack)) {
+            if ((centerY > playerY - ballRadius) && (centerY < playerY + playerPaddleHeight + ballRadius)) {
+                ping2.play();
+                direction = forward;
                 angle = angle + ((opponentY - centerY - 50) * 0.001);
+                speed++;
                 if (speed > maxSpeed) {
                     speed = maxSpeed;
                 }
             }
         }
-        else if (centerX < -40) {
+        else if (centerX < -ballDiameter) {
             opponentScored();
         }
     }
@@ -95,18 +112,15 @@ function render() {
     ctx.beginPath();
     //Player
     ctx.fillStyle = '#FFFFFF';
-    ctx.fillRect(100, playerY, 20, 100);
+    ctx.fillRect(playerPaddleBack, playerY, playerPaddleWidth, playerPaddleHeight);
     //Opponent
     ctx.fillStyle = '#FFFFF';
-    ctx.fillRect(width - 120, opponentY, 20, 100);
+    ctx.fillRect(opponentPaddleFront, opponentY, opponentPaddleWidth, opponentPaddleHeight);
     //Ball
     ctx.beginPath();
-    ctx.arc(centerX, centerY, ballRadius, 0, 2 * Math.PI, false);
+    ctx.arc(centerX, centerY, ballRadius, startAngle, endAngle, false);
     ctx.fillStyle = '#FFFFFF';
     ctx.fill();
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = '#FFFFF';
-    ctx.stroke();
 
     requestAnimationFrame(render);
 }
@@ -136,15 +150,15 @@ function opponentScored() {
 function startPoint() {
     centerX = width / 2;
     centerY = height / 2;
-    playerY = centerY - 50;
-    opponentY = playerY;
-    speed = 1;
+    playerY = centerY - playerPaddleHeight / 2;
+    opponentY = centerY - opponentPaddleHeight / 2;
+    speed = minSpeed;
     angle = Math.random() * (-0.2 - 0.2) + -0.2;
-    if (direction === 1) {
-        direction = -1;
+    if (direction === forward) {
+        direction = reverse;
     }
     else {
-        direction = 1;
+        direction = forward;
     }
 }
 
@@ -153,6 +167,11 @@ function resize() {
     ctx.canvas.height = canvas.clientHeight;
     width = canvas.clientWidth;
     height = canvas.clientHeight;
+
+    opponentPaddleFront = width - (paddleOffset + opponentPaddleWidth);
+    opponentPaddleBack = width - paddleOffset;
+    playerPaddleFront = paddleOffset + playerPaddleWidth;
+    playerPaddleBack = paddleOffset;
+
     resetGame();
-    //console.log("Width:" + width + " Height:" + height);
 }
